bf8580715bea56759e91f490e2e03553280f8d65
Seán Mac Gillicuddy <seantheappdev@gmail.com>
2026-07-30 09:38:37 +0100

    Load bottom reader navigation

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,6 +1,7 @@
 await import('./auth-fallback.js');
 await import('./reader-search.js');
 await import('./harvard.js');
+await import('./reader-bottom-nav.js');
 await import('./reader-behaviour.js');
 await import('./readable-text.js');
 await import('./library.js');
